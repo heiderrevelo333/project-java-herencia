2cbadc99c3fbb74b72e77ecb361daadb113bc24d
Esteban Revelo <estebanadso21@gmail.com>
2025-05-27 20:30:48 -0500

    Comportamiento: verificarPresion del Neumatico

--- src/App.java
+++ src/App.java
@@ -4,6 +4,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         Motor motor = new Motor("MTR123", "Toyota", 4, 150);
         Transmision transmision = new Transmision("TRS456", "Honda", "Manual", 6);
+        Neumatico neumatico = new Neumatico("N001", "Michelin", 17, 32.5);
 
         System.out.println("================= Motor =================");
         motor.mostrarInformacion();
@@ -12,6 +13,10 @@ public class App {
         System.out.println("\n================= Transmisión =================");
         transmision.mostrarInformacion();
         transmision.cambiarMarcha(3);
+
+        System.out.println("\n================= Neumático =================");
+        neumatico.mostrarInformacion();
+        neumatico.verificarPresion();   
 
     
     }
