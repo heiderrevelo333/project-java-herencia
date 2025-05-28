f90f1e489e40e33b7076cc11a46de2918354f2b3
Esteban Revelo <estebanadso21@gmail.com>
2025-05-27 20:13:53 -0500

    Subclases especializadas con comportamientos particulares

--- src/App.java
+++ src/App.java
@@ -1,5 +1,12 @@
+import models.*; 
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Motor motor = new Motor("MTR123", "Toyota", 4, 150);
+        motor.mostrarInformacion();
+        motor.encenderMotor();
+
+        System.out.println();
+    
     }
 }
