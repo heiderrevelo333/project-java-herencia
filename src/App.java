f149845e3afb88a86944b098fa738e771d54ac75
Esteban Revelo <estebanadso21@gmail.com>
2025-05-27 20:26:53 -0500

    Comportamiento: cambiarMarcha(int marcha)

--- src/App.java
+++ src/App.java
@@ -3,10 +3,16 @@ import models.*;
 public class App {
     public static void main(String[] args) throws Exception {
         Motor motor = new Motor("MTR123", "Toyota", 4, 150);
+        Transmision transmision = new Transmision("TRS456", "Honda", "Manual", 6);
+
+        System.out.println("================= Motor =================");
         motor.mostrarInformacion();
         motor.encenderMotor();
 
-        System.out.println();
+        System.out.println("\n================= Transmisión =================");
+        transmision.mostrarInformacion();
+        transmision.cambiarMarcha(3);
+
     
     }
 }
